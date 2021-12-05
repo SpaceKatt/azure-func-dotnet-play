5c13cde380cb081eff0d3d433cae4075ec89de55
SpaceKatt <spacekattpoispin@gmail.com>
2021-12-05 13:06:01 -0800

    add product endpoint

---
+++ src/Functions/GetProduct.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFuncDotnetPlay
+{
+    class ProductRequest
+    {
+        public IList<double> sequence { get; set; }
+    }
+
+    class ProductResponse
+    {
+        public double product { get; set; }
+
+        public ProductResponse(double product) => this.product = product;
+    }
+
+    class GetProduct
+    {
+        public GetProduct() {}
+
+        [Function(nameof(GetProduct))]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData request, FunctionContext context)
+        {
+            var logger = context.GetLogger<ProductResponse>();
+
+            try {
+                var body = await new StreamReader(request.Body).ReadToEndAsync();
+                var requestData = JsonConvert.DeserializeObject<ProductRequest>(body);
+
+                double product = 1;
+                foreach (var num in requestData.sequence)
+                {
+                    product *= num;
+                }
+
+                var response = request.CreateResponse(System.Net.HttpStatusCode.OK);
+                var responseData = JsonConvert.SerializeObject(new ProductResponse(product));
+                await response.WriteStringAsync(responseData).ConfigureAwait(false);
+
+                return response;
+
+            }
+            catch
+            {
+                var response = request.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                return response;
+            }
+        }
+
+    }
+}
